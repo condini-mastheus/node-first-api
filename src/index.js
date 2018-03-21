9d6c3a2216deaab949d7f4163f276f5b8ef1160c
unknown <matheus.condini@gmail.com>
2018-03-21 12:04:37 -0300

    Create folders and BD config, add first route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const controllers = require('./app/controllers/');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+
 const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('<code>Version: 1.0.0</code>');
+});
 
 controllers(app);
 
