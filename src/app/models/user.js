f835f81d127a8504589c4aeb657bd7e7b800b14d
unknown <matheus.condini@gmail.com>
2018-03-23 10:05:37 -0300

    Fix anonymous function with name

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-userSchema.pre('save', async function name(next) {
+userSchema.pre('save', async function (next) {
 	const hash = await bcrypt.hash(this.password, 10);
 	this.password = hash;
 
